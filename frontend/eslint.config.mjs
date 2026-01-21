0b5f5695c2c8a41e4998b109d08340509d6f23dc
Dylan Reimer <reimer.dylan@gmail.com>
2026-01-20 19:53:09 -0800

    fixed config issues for eslint

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,9 +1,18 @@
 import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+export default [
+  js.configs.recommended,
   pluginReact.configs.flat.recommended,
-]);
+  {
+    files: ["**/*.{js,jsx}"],
+    languageOptions: { 
+      globals: globals.browser 
+    },
+    rules: {
+    }
+  },
+  eslintConfigPrettier
+];
