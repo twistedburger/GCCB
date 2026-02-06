83a78bd8790320797b8eb419fa3dbd32086dd627
Claudia Le <cle54@my.bcit.ca>
2026-02-06 12:06:40 -0800

    Added callback support

--- frontend/src/components/JoinLeaveButton.jsx
+++ frontend/src/components/JoinLeaveButton.jsx
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types'
 
-export default function JoinLeaveButton({ isJoined }) {
+export default function JoinLeaveButton({ isJoined, onClick }) {
   return (
     <button
       type="button"
+      onClick={onClick}
       className={`m-2 px-8 py-1 rounded-xl bg-blue-secondary font-medium text-text-secondary ${!isJoined && 'outline outline-text-secondary'}`}
     >
       {isJoined ? 'Leave' : 'Join'}
@@ -11,4 +12,7 @@ export default function JoinLeaveButton({ isJoined }) {
   )
 }
 
-JoinLeaveButton.propTypes = { isJoined: PropTypes.bool }
+JoinLeaveButton.propTypes = {
+  isJoined: PropTypes.bool,
+  onClick: PropTypes.func,
+}
