7124bbe45cbb9276f1aa8f97d58e3394169c404e
Aaron <atsang59@my.bcit.ca>
2026-02-08 10:13:07 -0800

    Add Analytics, Profile, and Settings routes to App component; update Dashboard links

--- frontend/src/pages/dashboard/Dashboard.jsx
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -1,8 +1,21 @@
+import { Link } from 'react-router-dom'
+
 function Dashboard() {
   return (
-    <div>
+    <>
       <h2>This is Dashboard page!</h2>
-    </div>
+      <ul>
+        <li>
+          <Link to="/dashboard/analytics">Go to Analytics</Link>
+        </li>
+        <li>
+          <Link to="/dashboard/profile">Go to My Account</Link>
+        </li>
+        <li>
+          <Link to="/dashboard/settings">Go to Settings</Link>
+        </li>
+      </ul>
+    </>
   )
 }
 
