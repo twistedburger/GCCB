2eb51dc708fd7e1351d0bb2ea0c5c931f1705348
Claudia Le <cle54@my.bcit.ca>
2026-02-08 12:23:13 -0800

    Edit Home.jsx to include slider card component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import SearchBar from '../components/SearchBar'
 import staticMap from '../assets/static-map.jpg'
+import SliderCard from '../components/SliderCard'
 
 function Home() {
   return (
-    <div>
-      <img src={staticMap} className="absolute w-full h-full object-cover" />{' '}
-      {/*image takes long to load, just set src to empty string if not needed*/}
+    <div className="relative w-full h-full">
+      <img src={staticMap} className="absolute w-full h-full object-cover" />
       <SearchBar />
+      <SliderCard />
     </div>
   )
 }
