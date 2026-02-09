8a78f0c43c43540c761277c3299b4a78425720db
Dylan Reimer <reimer.dylan@gmail.com>
2026-02-09 11:27:44 -0800

    Add auth0 to backend

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,26 @@
 const express = require('express')
+const { auth } = require('express-openid-connect')
+
 const app = express()
 const db = require('./db')
 const port = 3000
 
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.AUTH0_SECRET,
+  baseURL: 'http://localhost:3000',
+  clientID: process.env.AUTH0_CLIENT_ID,
+  issuerBaseURL: process.env.AUTH0_DOMAIN,
+  // authorizationParams: {
+  //     connection: 'google-oauth2',
+  //   },
+}
+
+app.use(auth(config))
+
 app.get('/', (req, res) => {
-  res.send('Welcome to GCCB!')
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out')
 })
 
 app.get('/sample_query', (req, res) => {
