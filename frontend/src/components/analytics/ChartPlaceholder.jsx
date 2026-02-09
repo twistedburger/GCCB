af9a2da94d4c9c3fbf711cca35eb43e863256c97
Aaron <atsang59@my.bcit.ca>
2026-02-09 00:07:27 -0800

    Implement ChartPlaceholder component with label and height props with default values

--- frontend/src/components/analytics/ChartPlaceholder.jsx
+++ frontend/src/components/analytics/ChartPlaceholder.jsx
@@ -1,4 +1,31 @@
-function ChartPlaceholder() {
-  return <h1>Placeholder</h1>
+import PropTypes from 'prop-types'
+
+function ChartPlaceholder({ label, height }) {
+  return (
+    <div
+      style={{
+        height,
+        border: '1px dashed #ddd',
+        borderRadius: 10,
+        display: 'flex',
+        alightItems: 'center',
+        justifyContent: 'center',
+        fontSize: 12,
+        opacity: 0.85,
+      }}
+    >
+      {label}
+    </div>
+  )
 }
+
+ChartPlaceholder.propTypes = {
+  label: PropTypes.string.isRequired,
+  height: PropTypes.number,
+}
+
+ChartPlaceholder.defaultProps = {
+  height: 200,
+}
+
 export default ChartPlaceholder
