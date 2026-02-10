3ea6f3c6fe35b64a5a9d405f80f29912ba085240
Claudia Le <cle54@my.bcit.ca>
2026-02-10 15:31:45 -0800

    Edit bugged dragging motion

--- frontend/src/components/SliderCard.jsx
+++ frontend/src/components/SliderCard.jsx
@@ -1,15 +1,17 @@
 import { motion, useAnimation, useDragControls } from 'framer-motion'
 import PropTypes from 'prop-types'
 
-export default function SliderCard({ sliderInformation }) {
+export default function SliderCard({ children }) {
   const controls = useAnimation()
   const dragControls = useDragControls()
 
+  const handleHeight = 124
+
   const handleDragEnd = (event, info) => {
     if (info.offset.y < -100) {
-      controls.start({ y: 30 })
+      controls.start({ y: '5%' })
     } else {
-      controls.start({ y: 750 })
+      controls.start({ y: `calc(100% - ${handleHeight}px)` })
     }
   }
 
@@ -18,26 +20,30 @@ export default function SliderCard({ sliderInformation }) {
       drag="y"
       dragControls={dragControls}
       dragListener={false}
-      dragConstraints={{ top: 0, bottom: 750 }}
-      initial={{ y: 750 }}
+      dragConstraints={{ top: 0, bottom: 800 }}
+      initial={{ y: `calc(100% - ${handleHeight}px)` }}
       animate={controls}
       onDragEnd={handleDragEnd}
       dragMomentum={false}
       transition={{ type: 'spring', damping: 50, stiffness: 400 }}
-      className="bg-background-off-white h-screen w-full rounded-t-4xl fixed top-0 flex flex-col overflow-hidden z-12"
+      className="bg-background-off-white h-screen w-full rounded-t-4xl fixed top-0 flex flex-col overflow-hidden z-20"
     >
       <div
-        onPointerDown={e => dragControls.start(e)}
-        className="flex justify-center mt-6 h-14 w-full shrink-0 cursor-grab active:cursor-grabbing touch-none"
+        onPointerDown={e => {
+          e.preventDefault()
+          dragControls.start(e)
+          e.stopPropagation()
+        }}
+        className="flex justify-center h-16 w-full shrink-0 cursor-grab active:cursor-grabbing touch-none z-20"
       >
-        <div className="bg-text-primary rounded-full h-1.5 w-20"></div>
+        <div className="bg-text-primary rounded-full h-1.5 w-20 mt-6"></div>
       </div>
 
       <div className="flex-1 overflow-y-auto px-6 pb-32 flex flex-col gap-4">
-        {sliderInformation}
+        {children}
       </div>
     </motion.div>
   )
 }
 
-SliderCard.propTypes = { sliderInformation: PropTypes.node }
+SliderCard.propTypes = { children: PropTypes.node }
