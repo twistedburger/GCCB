c34719f4849fa41efb997fb8eadbf0d7227e54ca
Aaron <atsang59@my.bcit.ca>
2026-02-20 03:12:37 -0800

    Remove unused modes and emissions constants for walking and biking

--- backend/src/constants/emissions.js
+++ backend/src/constants/emissions.js
@@ -4,20 +4,8 @@ const EMISSIONS_G_PER_PASSENGER_KM = {
   CAR_PETROL: 170,
   CAR_ELECTRIC: 47,
   BUS: 97,
-  WALK: 0,
-  BIKE: 0,
-}
-
-const MODE = {
-  WALK: 'walk',
-  CYCLE: 'cycle',
-  BUS: 'bus',
-  CAR_ELECTRIC: 'electric_car',
-  CARPOOL: 'carpool',
-  CAR: 'car', // petrol will become baseline
 }
 
 module.exports = {
   EMISSIONS_G_PER_PASSENGER_KM,
-  MODE,
 }
