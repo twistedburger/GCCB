7507e53e298b68127d6d3191f88e756ef7963371
Aaron <atsang59@my.bcit.ca>
2026-03-04 04:25:20 -0800

    Refactor emissions constants to separate vehicle and transit factors

--- backend/src/constants/emissions.js
+++ backend/src/constants/emissions.js
@@ -1,11 +1,16 @@
 // src: https://ourworldindata.org/travel-carbon-footprint#:~:text=He%20estimates%2025%20grams%20CO,International%20Council%20on%20Clean%20Transportation.
 // Move to ReadMe eventually
-const EMISSIONS_G_PER_PASSENGER_KM = {
-  CAR_PETROL: 170,
-  CAR_ELECTRIC: 47,
-  BUS: 97,
+
+// Car factors are PER VEHICLE-KM.
+// Transit factors are PER PASSENGER-KM.
+const EMISSIONS_G_PER_KM = {
+  CAR_VEHICLE: {
+    PETROL: 170,
+    ELECTRIC: 47,
+  },
+  TRANSIT_PASSENGER: {
+    BUS: 97,
+  },
 }
 
-module.exports = {
-  EMISSIONS_G_PER_PASSENGER_KM,
-}
+module.exports = { EMISSIONS_G_PER_KM }
