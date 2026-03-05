989ba89a76a99014128da198469fbf9c2896f57f
yeali <ykim229@my.bcit.ca>
2026-03-05 11:34:31 -0800

    Update SearchBar to take optional placeHolder and styling for enhanced reusability

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -2,24 +2,29 @@ import PropTypes from 'prop-types'
 import { SearchOutlined } from '@mui/icons-material'
 import { useState } from 'react'
 
-export default function SearchBar({ onSearch }) {
+export default function SearchBar({
+  placeHolder = 'Search location...',
+  className = 'w-full flex flex-row justify-start focus-within:border-blue-500 focus-within:ring-4 focus-within:ring-blue-100 bg-white rounded-xl px-2 py-3 shadow-md shadow-light-grey transition-all',
+  onSearch,
+}) {
   const [location, setLocation] = useState('')
-
   return (
-    <div className="absolute top-12 left-1/2 -translate-x-1/2 z-10 w-9/10">
-      <div className="w-full flex flex-row justify-start bg-white rounded-xl px-2 py-3 shadow-md shadow-light-grey">
-        <input
-          type="text"
-          placeholder="Search location..."
-          onChange={e => setLocation(e.target.value)}
-          className="flex-1 pl-2 focus:outline-none"
-        />
-        <button onClick={() => onSearch(location)}>
-          <SearchOutlined className="text-text-primary" />
-        </button>
-      </div>
+    <div className={className}>
+      <input
+        type="text"
+        placeholder={placeHolder}
+        onChange={e => setLocation(e.target.value)}
+        className="flex-1 pl-2 focus:outline-none bg-transparent"
+      />
+      <button onClick={() => onSearch(location)}>
+        <SearchOutlined className="text-text-primary" />
+      </button>
     </div>
   )
 }
 
-SearchBar.propTypes = { onSearch: PropTypes.func }
+SearchBar.propTypes = {
+  placeHolder: PropTypes.string,
+  className: PropTypes.string,
+  onSearch: PropTypes.func,
+}
