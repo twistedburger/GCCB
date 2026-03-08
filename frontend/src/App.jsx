7912c2d70c86f4c7fc0029c7466c14f47a1fa034
yeali <ykim229@my.bcit.ca>
2026-03-07 17:39:50 -0800

    Display navbar when user authenticated and logged in

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import NavigationBar from './components/NavigationBar'
+import Sidebar from './components/Sidebar'
 import Home from './pages/Home'
 import MyTrip from './pages/MyTrips'
 import Dashboard from './pages/dashboard/Dashboard'
@@ -12,6 +12,7 @@ import TripFrequency from './pages/dashboard/analytics/TripFrequency'
 import Activity from './pages/dashboard/analytics/Activity'
 import Login from './pages/Login'
 import CreateUser from './pages/CreateUser'
+import UserGuide from './pages/UserGuide'
 import { useState, useEffect } from 'react'
 import { authLevel, AuthProvider } from './utils/Authorization'
 import ProtectedRoute from './components/ProtectedRoute'
@@ -44,57 +45,59 @@ function App() {
   return (
     <Router>
       <AuthProvider>
-        <div className="app-container h-screen">
+        <div className="app-container min-h-screen">
           {/* pages */}
-          <main className="content bg-background-off-white">
-            <Routes>
-              <Route
-                path="/"
-                element={
-                  !userAuthenticated ? (
-                    <Login />
-                  ) : !currentUser ? (
-                    <CreateUser
-                      ssoUser={ssoProfile}
-                      onUserCreated={setCurrentUser}
-                    />
-                  ) : (
-                    <Home />
-                  )
-                }
-              />
-              <Route
-                element={
-                  <ProtectedRoute requiredAuthorization={authLevel.USER} />
-                }
-              >
-                <Route path="/mytrip" element={<MyTrip />} />
-                <Route path="/dashboard" element={<Dashboard />} />
+          <div className="relative w-full min-h-screen flex bg-background-off-white">
+            {userAuthenticated && currentUser && <Sidebar />}{' '}
+            <main className="flex-1 overflow-y-auto">
+              <Routes>
+                <Route
+                  path="/"
+                  element={
+                    !userAuthenticated ? (
+                      <Login />
+                    ) : !currentUser ? (
+                      <CreateUser
+                        ssoUser={ssoProfile}
+                        onUserCreated={setCurrentUser}
+                      />
+                    ) : (
+                      <Home />
+                    )
+                  }
+                />
+                <Route
+                  element={
+                    <ProtectedRoute requiredAuthorization={authLevel.USER} />
+                  }
+                >
+                  <Route path="/mytrip" element={<MyTrip />} />
+                  <Route path="/dashboard" element={<Dashboard />} />
 
-                <Route path="/dashboard/analytics" element={<Analytics />} />
-                <Route
-                  path="/dashboard/analytics/co2-savings"
-                  element={<Co2Savings />}
-                />
-                <Route
-                  path="/dashboard/analytics/commutes"
-                  element={<Commutes />}
-                />
-                <Route
-                  path="/dashboard/analytics/trip-frequency"
-                  element={<TripFrequency />}
-                />
-                <Route
-                  path="/dashboard/analytics/activity"
-                  element={<Activity />}
-                />
-
-                <Route path="/dashboard/profile" element={<Profile />} />
-                <Route path="/dashboard/settings" element={<Settings />} />
-              </Route>
-            </Routes>
-          </main>
-          <NavigationBar />
+                  <Route path="/dashboard/analytics" element={<Analytics />} />
+                  <Route
+                    path="/dashboard/analytics/co2-savings"
+                    element={<Co2Savings />}
+                  />
+                  <Route
+                    path="/dashboard/analytics/commutes"
+                    element={<Commutes />}
+                  />
+                  <Route
+                    path="/dashboard/analytics/trip-frequency"
+                    element={<TripFrequency />}
+                  />
+                  <Route
+                    path="/dashboard/analytics/activity"
+                    element={<Activity />}
+                  />
+                  <Route path="/dashboard/profile" element={<Profile />} />
+                  <Route path="/dashboard/settings" element={<Settings />} />
+                  <Route path="/user-guide" element={<UserGuide />} />
+                </Route>
+              </Routes>
+            </main>
+          </div>
         </div>
       </AuthProvider>
     </Router>
