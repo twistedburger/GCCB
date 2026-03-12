4282e4e721cceb3f585933af96461a9cf309279c
yeali <ykim229@my.bcit.ca>
2026-03-12 11:53:49 -0700

    Add disabled property to LocationSearch

--- frontend/src/components/LocationSearch.jsx
+++ frontend/src/components/LocationSearch.jsx
@@ -6,6 +6,7 @@ export default function LocationSearch({
   defaultLocation = '',
   placeHolder = 'Search location...',
   className = 'w-full flex flex-row justify-start focus-within:ring-4 focus-within:ring-blue-100 border border-transparent rounded-xl shadow-md shadow-light-grey transition-all',
+  disabled = false,
 }) {
   const [location, setLocation] = useState(defaultLocation)
   const autocompleteRef = useRef(null)
@@ -40,7 +41,7 @@ export default function LocationSearch({
 
   return (
     <gmp-place-autocomplete
-      className={`w-full ${className}`}
+      className={`w-full ${className} ${disabled ? 'pointer-events-none opacity-50' : ''}`}
       ref={autocompleteRef}
       placeholder={placeHolder}
       onInput={e => setLocation(e.target.value)}
@@ -56,4 +57,5 @@ LocationSearch.propTypes = {
   defaultLocation: PropTypes.string,
   placeHolder: PropTypes.string,
   className: PropTypes.string,
+  disabled: PropTypes.bool,
 }
