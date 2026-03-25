9638c13d22779b099d32c11cdf6b52c6d5e1751f
yeali <ykim229@my.bcit.ca>
2026-03-25 10:43:32 -0700

    Removed unnecessary inline comment

--- backend/db/scripts/add_created_at_column_event_and_route.sql
+++ backend/db/scripts/add_created_at_column_event_and_route.sql
@@ -1,5 +1,5 @@
 ALTER TABLE "event" 
 ADD COLUMN IF NOT EXISTS created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
 
-ALTER TABLE "route" -- Note: your previous backend code called this "routes" (plural)
+ALTER TABLE "route"
 ADD COLUMN IF NOT EXISTS created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
