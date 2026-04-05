5ff399377607810ab407be6dd6e8f523e1cc3629
Dylan Reimer <reimer.dylan@gmail.com>
2026-04-05 09:20:49 -0700

    Add JSDoc

--- frontend/src/pages/CreateUser.jsx
+++ frontend/src/pages/CreateUser.jsx
@@ -1,6 +1,13 @@
 import { PropTypes } from 'prop-types'
 import ProfileForm from '../components/ProfileForm'
 
+/**
+ * Create user page as a version of profile form for a new user.
+ *
+ * @param {Object} ssoUser user returned from the auth0 authentication
+ * @param {func} onUserCreated callback function once user has been created and added to the database successfully
+ * @returns {JSX.Element}
+ */
 function CreateUser({ ssoUser, onUserCreated }) {
   const insertUser = async formData => {
     const response = await fetch('http://localhost:3000/createNewUser', {
