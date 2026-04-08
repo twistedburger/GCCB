8c084fc3a9af166766f6ea59c5d9240c1018e797
Claudia Le <cle54@my.bcit.ca>
2026-04-07 18:25:27 -0700

    Move user strings to localization script for RouteCard component

---
+++ frontend/src/locales/en/ComponentStrings/RouteCardStrings.js
@@ -0,0 +1,9 @@
+export const routeCardStrings = {
+  peopleGoing: count => `${count} people going`,
+  seatsLeft: count => `(${count} seats left)`,
+  completed: 'Completed:',
+  report: 'Report',
+  leave: 'Leave',
+  join: 'Join',
+  at: '@',
+}
