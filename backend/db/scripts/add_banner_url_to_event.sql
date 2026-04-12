6137709618092759c8b6217775af4144731f1def
Aaron <atsang59@my.bcit.ca>
2026-04-11 18:16:56 -0700

    Remove old db scripts

--- backend/db/scripts/add_banner_url_to_event.sql
+++
@@ -1,7 +0,0 @@
--- Save banner urls, save placeId to update banner_url when url expires
-ALTER TABLE "event" ADD COLUMN IF NOT EXISTS banner_url TEXT;
-ALTER TABLE "event" ADD COLUMN IF NOT EXISTS place_id VARCHAR(50);
-
-UPDATE event SET place_id = 'ChIJ4S7wxnhxhlQRFGfmMvQ74LQ' WHERE id = 1;
-UPDATE event SET place_id = 'ChIJnZHwi2NxhlQRN3CYHzc3giE' WHERE id = 2;
-UPDATE event SET place_id = 'ChIJ5f5T_SF3hlQRnRB6ZAeyWjU' WHERE id = 3;
