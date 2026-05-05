ab3c810f91cc0977a02e4239180ab8bb436a1db6
Dylan Reimer <reimer.dylan@gmail.com>
2026-05-05 16:46:15 -0700

    Write unit test for no notification type

--- backend/src/utils/__tests__/NotificationQueries.test.js
+++ backend/src/utils/__tests__/NotificationQueries.test.js
@@ -1,22 +1,26 @@
-const db = require('../../db')
+const { serverStrings } = require('../../../locales/en/serverLocales')
+const { insertNotification } = require('../NotificationQueries')
 
 describe('Test insertNotification database query', () => {
-  jest.mock('../../db', () => ({
-    query: jest
-      .fn()
-      .mockResolvedValue({ rows: [{ reported: 0 }], rowCount: 0 }),
-    pool: {
-      connect: jest.fn(),
-    },
-  }))
-
-  test('Notification type not set throws error', () => {
-    db.query('')
+  test('Notification type not set throws error', async () => {
+    const notification = { metadata: { message: 'new notification' } }
+    await expect(insertNotification(notification)).rejects.toThrow(
+      `${serverStrings.errors.notificationError} undefined`
+    )
   })
 
   test('Invalid notification type throws error', () => {})
 
-  test('Notification inserted using event query strings', () => {})
+  test('Notification inserted using event query strings', () => {
+    jest.mock('../../../db', () => ({
+      query: jest
+        .fn()
+        .mockResolvedValue({ rows: [{ reported: 0 }], rowCount: 0 }),
+      pool: {
+        connect: jest.fn(),
+      },
+    }))
+  })
 
   test('Notification inserted using route query strings', () => {})
 
@@ -24,7 +28,5 @@ describe('Test insertNotification database query', () => {
 
   test('Notification inserted using message query strings', () => {})
 
-  test(
-    'Insertion into user_notification table queried with appropriate query string'
-  )
+  test('Insertion into user_notification table queried with appropriate query string', () => {})
 })
