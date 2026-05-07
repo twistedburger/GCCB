37c61b2f32016426cfc742e92c78fb2b401a1b94
Claudia Le <cle54@my.bcit.ca>
2026-05-06 18:20:08 -0700

    Add documentation to ProfilePicture component

--- frontend/src/components/ProfilePicture.jsx
+++ frontend/src/components/ProfilePicture.jsx
@@ -2,13 +2,23 @@ import PropTypes from 'prop-types'
 import GenericButton from './GenericButton'
 import { Person, Edit } from '@mui/icons-material'
 
-function ProfilePicture({ onImageClick, avatarUrl, canEdit = true }) {
+/**
+ * A circular profile picture component with an edit overlay button.
+ *
+ * Displays the user's avatar if a URL is provided, or a default person icon
+ * as a placeholder. An edit icon is always shown in the bottom-right corner
+ * to indicate the image is clickable/changeable.
+ *
+ * @param {Function} [onImageClick] - Callback triggered when the user clicks the profile picture.
+ * @param {string} [props.avatarUrl] - URL of the user's current profile image.
+ * @returns {JSX.Element}
+ */
+function ProfilePicture({ onImageClick, avatarUrl }) {
   return (
     <GenericButton
       onClick={onImageClick}
       unstyled={true}
       customStyling="relative h-full w-full rounded-full border border-light-grey bg-light-grey transition active:scale-95"
-      disabled={!canEdit}
     >
       <div className="h-full w-full overflow-hidden rounded-full">
         {avatarUrl && avatarUrl.length > 0 ? (
@@ -23,11 +33,9 @@ function ProfilePicture({ onImageClick, avatarUrl, canEdit = true }) {
           </div>
         )}
       </div>
-      {canEdit && (
-        <div className="absolute bottom-0 right-0 flex items-center justify-center h-7 w-7 rounded-full bg-white border border-light-grey shadow-sm text-text-primary">
-          <Edit sx={{ fontSize: 16 }} />
-        </div>
-      )}
+      <div className="absolute bottom-0 right-0 flex items-center justify-center h-7 w-7 rounded-full bg-white border border-light-grey shadow-sm text-text-primary">
+        <Edit sx={{ fontSize: 16 }} />
+      </div>
     </GenericButton>
   )
 }
@@ -35,7 +43,6 @@ function ProfilePicture({ onImageClick, avatarUrl, canEdit = true }) {
 ProfilePicture.propTypes = {
   onImageClick: PropTypes.func,
   avatarUrl: PropTypes.string,
-  canEdit: PropTypes.bool,
 }
 
 export default ProfilePicture
