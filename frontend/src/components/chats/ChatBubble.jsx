fad297702d2320ba47e49cab649c3529bab2a54c
yeali <ykim229@my.bcit.ca>
2026-05-11 13:28:20 -0700

    Add Y,M,D to chat bubble

--- frontend/src/components/chats/ChatBubble.jsx
+++ frontend/src/components/chats/ChatBubble.jsx
@@ -24,6 +24,9 @@ export default function ChatBubble({ message, isMe }) {
         </span>
         <span className="text-[9px] text-dark-grey uppercase">
           {new Date(message.sent_at).toLocaleTimeString([], {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
             hour: '2-digit',
             minute: '2-digit',
           })}
