5490b2e5377a4fa24e9add35322f27692eddf2c7
yeali <ykim229@my.bcit.ca>
2026-05-11 13:58:39 -0700

    naming convention

--- backend/src/services/ChatServices.js
+++ backend/src/services/ChatServices.js
@@ -1,7 +1,7 @@
 const { pool } = require('../../db.js')
 const {
   chatServiceStrings,
-} = require('../../locales/en/service/ChatServicesLocales.js')
+} = require('../../locales/en/services/ChatServicesLocales.js')
 
 /**
  * Closes expired rooms and deletes archived ones.
