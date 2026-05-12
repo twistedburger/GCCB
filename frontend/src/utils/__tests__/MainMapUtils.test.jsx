9693d94face83c423a1ab1b61f0ccc167eb7be79
Claudia Le <cle54@my.bcit.ca>
2026-05-12 09:23:13 -0700

    Create unit tests for new util in MainMapUtils

--- frontend/src/utils/__tests__/MainMapUtils.test.jsx
+++ frontend/src/utils/__tests__/MainMapUtils.test.jsx
@@ -1,5 +1,5 @@
 import { render } from '@testing-library/react'
-import { postGISToLatLng, RadiusCircle } from '../MainMapUtils'
+import { postGISToLatLng, reverseGeocode, RadiusCircle } from '../MainMapUtils'
 
 // postGISToLatLng
 describe('postGISToLatLng', () => {
@@ -16,6 +16,145 @@ describe('postGISToLatLng', () => {
     const result = postGISToLatLng(vancouverHex)
     expect(result).toHaveProperty('lat')
     expect(result).toHaveProperty('lng')
+  })
+})
+
+//reverseGeocode
+describe('reverseGeocode', () => {
+  const mockGeocode = jest.fn()
+
+  beforeEach(() => {
+    global.google = {
+      maps: {
+        Geocoder: jest.fn(() => ({ geocode: mockGeocode })),
+      },
+    }
+  })
+
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
+
+  const buildComponents = ({
+    streetNumber = '',
+    route = '',
+    locality = '',
+    adminArea = '',
+    postalCode = '',
+    country = '',
+  }) => [
+    {
+      types: ['street_number'],
+      long_name: streetNumber,
+      short_name: streetNumber,
+    },
+    { types: ['route'], long_name: route, short_name: route },
+    { types: ['locality'], long_name: locality, short_name: locality },
+    {
+      types: ['administrative_area_level_1'],
+      long_name: adminArea,
+      short_name: adminArea,
+    },
+    { types: ['postal_code'], long_name: postalCode, short_name: postalCode },
+    { types: ['country'], long_name: country, short_name: country },
+  ]
+
+  test('returns formatted address as [street, city line, country]', async () => {
+    mockGeocode.mockResolvedValue({
+      results: [
+        {
+          address_components: buildComponents({
+            streetNumber: '777',
+            route: 'Pacific Blvd',
+            locality: 'Vancouver',
+            adminArea: 'BC',
+            postalCode: 'V6B 4Y8',
+            country: 'Canada',
+          }),
+        },
+      ],
+    })
+
+    const result = await reverseGeocode({ lat: 49.2827, lng: -123.1207 })
+
+    expect(result).toEqual([
+      '777 Pacific Blvd',
+      'Vancouver, BC, V6B 4Y8',
+      'Canada',
+    ])
+  })
+
+  test('throws an error when geocoding returns no results', async () => {
+    mockGeocode.mockResolvedValue({ results: [] })
+
+    await expect(reverseGeocode({ lat: 0, lng: 0 })).rejects.toThrow()
+  })
+
+  test('omits street line when street number and route are missing', async () => {
+    mockGeocode.mockResolvedValue({
+      results: [
+        {
+          address_components: buildComponents({
+            locality: 'Vancouver',
+            adminArea: 'BC',
+            postalCode: 'V6B 4Y8',
+            country: 'Canada',
+          }),
+        },
+      ],
+    })
+
+    const result = await reverseGeocode({ lat: 49.2827, lng: -123.1207 })
+
+    expect(result).toEqual(['Vancouver, BC, V6B 4Y8', 'Canada'])
+  })
+
+  test('uses sublocality when locality is missing', async () => {
+    mockGeocode.mockResolvedValue({
+      results: [
+        {
+          address_components: [
+            {
+              types: ['sublocality'],
+              long_name: 'Downtown',
+              short_name: 'Downtown',
+            },
+            {
+              types: ['administrative_area_level_1'],
+              long_name: 'British Columbia',
+              short_name: 'BC',
+            },
+            {
+              types: ['postal_code'],
+              long_name: 'V6B 4Y8',
+              short_name: 'V6B 4Y8',
+            },
+            { types: ['country'], long_name: 'Canada', short_name: 'Canada' },
+          ],
+        },
+      ],
+    })
+
+    const result = await reverseGeocode({ lat: 49.2827, lng: -123.1207 })
+
+    expect(result[0]).toContain('Downtown')
+  })
+
+  test('omits missing fields from city line', async () => {
+    mockGeocode.mockResolvedValue({
+      results: [
+        {
+          address_components: buildComponents({
+            locality: 'Vancouver',
+            country: 'Canada',
+          }),
+        },
+      ],
+    })
+
+    const result = await reverseGeocode({ lat: 49.2827, lng: -123.1207 })
+
+    expect(result).toEqual(['Vancouver', 'Canada'])
   })
 })
 
