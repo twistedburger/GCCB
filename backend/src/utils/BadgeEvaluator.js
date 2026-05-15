ef343d74434edb0c058855dffd2e99270ce40be8
Aaron <atsang59@my.bcit.ca>
2026-05-15 07:34:26 -0700

    Rename files and moved to appropriate directories

--- backend/src/utils/BadgeEvaluator.js
+++ backend/src/utils/BadgeEvaluator.js
@@ -3,7 +3,7 @@
  * Handles badge achievement checks, progress updates, and retrieval of badge details.
  */
 class BadgeEvaluator {
-  /** @type {import('./BadgeQueries').BadgeQueries} */
+  /** @type {import('../services/BadgeServices').BadgeQueries} */
   #badgeQueries
 
   // defined metrics constants from db
@@ -31,7 +31,7 @@ class BadgeEvaluator {
 
   /**
    * @param {Object} params
-   * @param {import('./BadgeQueries').BadgeQueries} params.badgeQueries
+   * @param {import('../services/BadgeServices').BadgeQueries} params.badgeQueries
    */
   constructor({ badgeQueries }) {
     this.#badgeQueries = badgeQueries
